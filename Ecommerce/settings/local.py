e136c598d84a9c5fb88e499e786c8ba514f131d1
Festorz <festaskirui@gmail.com>
2021-05-15 07:30:01 +0300

    final

--- Ecommerce/settings/local.py
+++ Ecommerce/settings/local.py
@@ -14,6 +14,8 @@ import os
 
 import django_heroku
 
+from decouple import config
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 # BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -22,7 +24,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'qsu3g7h5rx#z(^0*(rx)fy1-eih+7w1+)y$1em*kgsgru#sv^l'
+
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
